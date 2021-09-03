2ef7c75167ecb5a88ced383cc7c568b45fac44ba
Felipe Marques de Almeida <felipemarques89@gmail.com>
2021-09-03 20:33:47 -0300

    Update cmd_build.sh

--- conda.recipe/cmd_build.sh
+++ conda.recipe/cmd_build.sh
@@ -4,3 +4,14 @@ conda clean -afy
 
 # build package
 conda-build --user falmeida . -c conda-forge -c bioconda -c anaconda -c defaults -c falmeida
+
+# convert to other SOs
+conda convert -p osx-64 $CONDA_PREFIX/conda-bld/linux-64/gff-toolbox-*.tar.bz2
+conda convert -p win-64 $CONDA_PREFIX/conda-bld/linux-64/gff-toolbox-*.tar.bz2
+
+# upload
+anaconda upload osx-64/* --force
+anaconda upload win-64/* --force
+
+# clean dir
+rm -rf osx-64 win-64
